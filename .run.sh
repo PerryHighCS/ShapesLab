cafe81a8794d8e1e6742d15abb5d15d6dff7a2b5
Brian Dahlem <dahlem.brian@cusd80.com>
2020-11-20 17:25:57 +0000

    cleaning up build file

--- .run.sh
+++ .run.sh
@@ -6,11 +6,11 @@ set -e # exit on errors
 echo compiling...
 
 # compile all class files into the build dir
-javac *.java \
-    -cp .:./lib/jcodec-0.2.5.jar:./lib/jcodec-javase-0.2.5.jar
+javac *.java -cp .:./lib/jcodec-0.2.5.jar:./lib/jcodec-javase-0.2.5.jar
+
+echo running...
 
 # run the packaged project
-java \
-    -cp .:./lib/jcodec-0.2.5.jar:./lib/jcodec-javase-0.2.5.jar \
+java -cp .:./lib/jcodec-0.2.5.jar:./lib/jcodec-javase-0.2.5.jar \
     Picture -D java.awt.headless=true 
     

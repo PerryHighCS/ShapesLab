18390d4e9019e19eb9f13c8e39484dcfb25ac55b
Brian Dahlem <dahlem.brian@cusd80.com>
2020-11-20 17:15:01 +0000

    Updated repl.it build system

---
+++ .run.sh
@@ -0,0 +1,16 @@
+export DISPLAY=
+
+# repl.it compile script for greenfoot projects
+set -e # exit on errors
+
+echo compiling...
+
+# compile all class files into the build dir
+javac *.java \
+    -cp .:./lib/jcodec-0.2.5.jar:./lib/jcodec-javase-0.2.5.jar
+
+# run the packaged project
+java \
+    -cp .:./lib/jcodec-0.2.5.jar:./lib/jcodec-javase-0.2.5.jar \
+    Picture -D java.awt.headless=true 
+    
--- make.sh
+++
@@ -1,3 +0,0 @@
-export DISPLAY=
-javac *.java -d . Picture.java
-java Picture -D java.awt.headless=true
